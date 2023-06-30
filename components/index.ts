b235f23b83dfc5c3d06844f46c31be4763625339
Nigam136 <patelnigam136@gmail.com>
2023-06-30 15:45:22 +0530

    first commit

---
+++ components/index.ts
@@ -0,0 +1,21 @@
+import Hero from "./Hero";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import CustomButton from "./CustomButton";
+import CustomFilter from "./CustomFilter";
+import SearchBar from "./SearchBar";
+import Searchmanufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
+
+export {
+  Hero,
+  Navbar,
+  Footer,
+  CustomButton,
+  CustomFilter,
+  SearchBar,
+  Searchmanufacturer,
+  CarCard,
+  CarDetails,
+};
